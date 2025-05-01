9baf3d0e561cee1a3c26b62cde9bdfb09f89a13d
JeYeongR <wpdud2003@gmail.com>
2025-05-01 12:40:51 +0900

    chore: initial project setup

---
+++ src/test/java/com/example/springaipractice/SpringAiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
